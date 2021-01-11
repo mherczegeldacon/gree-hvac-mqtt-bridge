2c4f41c66267d19e012e7f2c87052d5173809709
root <mherczeg@eldacon.hu>
2021-01-11 20:06:10 +0100

    third commit

--- run.sh
+++ run.sh
@@ -6,14 +6,14 @@ npm install
 npx pm2 start index.js --silent -m --merge-logs --name="HVAC_1" -- \
 	--hvac-host="10.0.88.38" \
 	--mqtt-broker-url="mqtt://10.0.88.1" \
-	--mqtt-topic-prefix="/greehvac/halo" \
+	--mqtt-topic-prefix="greehvac/halo" \
 	--mqtt-username="" \
 	--mqtt-password=""
 
 npx pm2 start index.js --silent -m --merge-logs --name="HVAC_2" -- \
 	--hvac-host="10.0.88.42" \
 	--mqtt-broker-url="mqtt://10.0.88.1" \
-	--mqtt-topic-prefix="/greehvac/nappali" \
+	--mqtt-topic-prefix="greehvac/nappali" \
 	--mqtt-username="" \
 	--mqtt-password=""
 
